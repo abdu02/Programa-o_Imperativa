a052675568a85148707103276b47b2922fc05892
abdu02 <78506547+abdu02@users.noreply.github.com>
2021-10-25 22:38:20 -0300

    exercicio_2_ aula3

---
+++ ex2.js
@@ -0,0 +1,51 @@
+{
+  let nome = "José";
+  let sobrenome = "da Silva";
+  let idade = 27;
+  let peso = 83.5;
+  let altura = 1.70;
+  let plano = "Ouro";
+
+  IMC = peso / ( altura * 2 );
+
+  console.log(nome + " tem " + idade + " anos e seu indice de corporal é de " + Math.round(IMC));
+}
+
+{
+    let nome = "Carlos";
+    let sobrenome = "de Souza";
+    let idade = 28;
+    let peso = 80.1;
+    let altura = 1.76;
+    let plano = "Diamante";
+  
+    IMC = peso / ( altura * 2 );
+  
+    console.log(nome + " tem " + idade + " anos e seu indice de corporal é de " + Math.round(IMC));
+  }
+
+  {
+    let nome = "Aline";
+    let sobrenome = "Ferreira";
+    let idade = 33;
+    let peso = 63.7;
+    let altura = 1.53;
+    let plano = "Nulo";
+  
+    IMC = peso / ( altura * 2 );
+  
+    console.log(nome + " tem " + idade + " anos e seu indice de corporal é de " + Math.round(IMC));
+  }
+
+  {
+    let nome = "Ana";
+    let sobrenome = "Paula";
+    let idade = 26;
+    let peso = 55.0;
+    let altura = 1.62;
+    let plano = "Prata";
+  
+    IMC = peso / ( altura * 2 );
+  
+    console.log(nome + " tem " + idade + " anos e seu indice de corporal é de " + Math.round(IMC));
+  }
